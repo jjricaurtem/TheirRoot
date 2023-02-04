07c5b659ad1dfaaf8265ed2d1c0c4beb3c31344c
John Ricaurte <jjricaurtem@gmail.com>
2023-02-03 20:30:54 -0500

    Level

---
+++ Assets/Scripts/TheirRoot/LevelUI.cs
@@ -0,0 +1,44 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TheirRoot
+{
+    public class LevelUI : MonoBehaviour
+    {
+        [SerializeField] private LevelEvents levelEvents;
+        [SerializeField] private GameObject uiFinalMessagePanel;
+        [SerializeField] private TMP_Text levelText;
+        private int _currentLevel = 1;
+
+        private void Awake()
+        {
+            levelEvents.EndGame += EndGame;
+            levelEvents.StartNewLevel += StartNewLevel;
+        }
+
+        private void StartNewLevel(LevelValues levelValues, int levelNumber)
+        {
+            _currentLevel = levelNumber;
+            levelText.text = $"Level: {levelNumber}";
+        }
+
+        private void Start()
+        {
+            uiFinalMessagePanel.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            levelEvents.EndGame -= EndGame;
+            levelEvents.StartNewLevel -= StartNewLevel;
+        }
+
+        private void EndGame()
+        {
+            uiFinalMessagePanel.SetActive(true);
+        }
+
+        public void Restart() => SceneManager.LoadScene("GameScene");
+    }
+}
