1ac74678f9827a0440d961553c2e3686618ac70a
John Ricaurte <jjricaurtem@gmail.com>
2023-02-04 13:06:23 -0500

    Tiles features complete

---
+++ Assets/Scripts/Editor/TileEditor.cs
@@ -0,0 +1,18 @@
+﻿using TheirRoot;
+using UnityEditor;
+using UnityEngine;
+
+namespace Scripts.Editor
+{
+    [CustomEditor(typeof(Tile))]
+    public class TileEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var tile = (Tile)target;
+            DrawDefaultInspector();
+
+            if (GUILayout.Button("Toggle Visual")) tile.ToggleVisualHex();
+        }
+    }
+}
