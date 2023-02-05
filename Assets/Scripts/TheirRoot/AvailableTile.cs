27701411a8e10aad71ed6bedbb74e429b3f612b2
John Ricaurte <jjricaurtem@gmail.com>
2023-02-05 18:46:38 -0500

    Root tile calculation

--- Assets/Scripts/TheirRoot/AvailableTile.cs
+++ Assets/Scripts/TheirRoot/AvailableTile.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -7,30 +9,62 @@ namespace TheirRoot
     public class AvailableTile : MonoBehaviour, IPointerClickHandler
     {
         [SerializeField] private TileMap tileMap;
+        [SerializeField] private TileEvents tileEvents;
         private Sprite _emptyImage;
         private Image _image;
         private RootPartSo _rootPartSo;
+        private List<Tile> _possibleTiles = new();
+        [SerializeField] private RootPart rootPartPrefab;
         public bool IsEmpty { get; private set; } = true;
-
+        
         private void Awake()
         {
             _image = GetComponent<Image>();
             _emptyImage = _image.sprite;
             IsEmpty = true;
+            tileEvents.OnTileClick += OnTileClick;
+        }
+
+        private void OnDestroy()
+        {
+            tileEvents.OnTileClick -= OnTileClick;
+        }
+
+        private void OnTileClick(Tile tile)
+        {
+            if (_possibleTiles.Count <= 0) return;
+            if (!_possibleTiles.Contains(tile)) return;
+
+            var rootPart = Instantiate(rootPartPrefab, tile.transform);
+            rootPart.rootPartSo = _rootPartSo;
+            rootPart.myRootSpriteRenderer.sprite = _rootPartSo.rootSprite;
+                    
+            tile.Item = rootPart;
+            rootPart.transform.localRotation = Quaternion.identity;
+            rootPart.currentTile = tile;
+            
+            foreach (var possibleTile in _possibleTiles)
+            {
+                possibleTile.ToggleVisualHex();
+            }
+            _possibleTiles.Clear();
+
+            FillTile(null);
         }
 
         public void OnPointerClick(PointerEventData eventData)
         {
+            _possibleTiles.Clear();
             if (eventData.button == PointerEventData.InputButton.Right)
             {
                 FillTile(null);
             }
             else if (eventData.button == PointerEventData.InputButton.Left)
             {
-                var possibleTiles =  tileMap.GetPossibleTiles(_rootPartSo);
-                foreach (var possibleTile in possibleTiles)
+                _possibleTiles = tileMap.GetPossibleTiles(_rootPartSo);
+                foreach (var possibleTile in _possibleTiles)
                 {
-                    possibleTile.Value.ToggleVisualHex();
+                    possibleTile.ToggleVisualHex();
                 }
             }
         }
--- Assets/Scripts/TheirRoot/TileMap.cs
+++ Assets/Scripts/TheirRoot/TileMap.cs
@@ -10,6 +10,9 @@ namespace TheirRoot
         public int rows = 8;
         public int cols = 8;
         public RootPart initialRootPartPrefab;
+
+
+        private readonly List<Tile> _tilesEvaluated = new();
         public Tile[][] HexMatrix { get; private set; }
         private Tile startTile { get; set; }
 
@@ -21,25 +24,6 @@ namespace TheirRoot
                 Destroy(HexMatrix[rowNum][colNum]);
 
             BuildTileMap();
-        }
-
-        public Dictionary<Direction, Tile> GetNeighbours(Tile tile)
-        {
-            var neighbours = new Dictionary<Direction, Tile>();
-            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
-            {
-                var offset = DirectionValues.GetOffSet(direction);
-                var neighbourRow = tile.row + offset[0];
-                var neighbourCol = tile.col + offset[1];
-
-                if (neighbourRow < 0 || neighbourRow >= rows) continue;
-                if (neighbourCol < 0 || neighbourCol >= cols) continue;
-
-                var neighbourTile = HexMatrix[neighbourRow][neighbourCol];
-                neighbours.Add(direction, neighbourTile);
-            }
-
-            return neighbours;
         }
 
         public void BuildTileMap()
@@ -79,45 +63,85 @@ namespace TheirRoot
             rootPart.currentTile = startTile;
         }
 
+        private Dictionary<Direction, Tile> GetNeighbours(Tile tile)
+        {
+            var neighbours = new Dictionary<Direction, Tile>();
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var offset = DirectionValues.GetOffSet(direction);
+                var neighbourRow = tile.row + offset[1];
+                var neighbourCol = tile.col + offset[0] + (neighbourRow % 2 != 0 ? -1 : 0);
+
+                if (neighbourRow < 0 || neighbourRow >= rows) continue;
+                if (neighbourCol < 0 || neighbourCol >= cols) continue;
+
+                var neighbourTile = HexMatrix[neighbourRow][neighbourCol];
+                neighbours.Add(direction, neighbourTile);
+            }
+
+            return neighbours;
+        }
 
         private void GetAllFreeJointTiles(Direction parentDirection, Tile currentTile,
-            List<KeyValuePair<Direction, Tile>> freeJoinTiles)
+            ICollection<KeyValuePair<Direction, Tile>> freeJoinTiles)
         {
             var item = currentTile.Item;
             var inverseParentDirection = DirectionValues.GetInverseDirection(parentDirection);
             if (item is not null && item.GetType() == typeof(RootPart))
             {
+                _tilesEvaluated.Add(currentTile);
                 var rootPart = (RootPart)item;
                 var neighbours = GetNeighbours(rootPart.currentTile);
 
-                if (rootPart.rootPartSo.NW && neighbours.TryGetValue(Direction.NorthWest, out var tileNW))
+                if (rootPart.rootPartSo.NW && neighbours.TryGetValue(Direction.NorthWest, out var tileNW) &&
+                    !_tilesEvaluated.Contains(tileNW))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.NorthWest), tileNW,
                         freeJoinTiles);
-                if (rootPart.rootPartSo.NE && neighbours.TryGetValue(Direction.NorthEast, out var tileNE))
+                if (rootPart.rootPartSo.NE && neighbours.TryGetValue(Direction.NorthEast, out var tileNE) &&
+                    !_tilesEvaluated.Contains(tileNE))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.NorthEast), tileNE,
                         freeJoinTiles);
-                if (rootPart.rootPartSo.E && neighbours.TryGetValue(Direction.East, out var tileE))
+                if (rootPart.rootPartSo.E && neighbours.TryGetValue(Direction.East, out var tileE) &&
+                    !_tilesEvaluated.Contains(tileE))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.East), tileE, freeJoinTiles);
-                if (rootPart.rootPartSo.SE && neighbours.TryGetValue(Direction.SouthEast, out var tileSE))
+                if (rootPart.rootPartSo.SE && neighbours.TryGetValue(Direction.SouthEast, out var tileSE) &&
+                    !_tilesEvaluated.Contains(tileSE))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.SouthEast), tileSE,
                         freeJoinTiles);
-                if (rootPart.rootPartSo.SW && neighbours.TryGetValue(Direction.SouthWest, out var tileSW))
+                if (rootPart.rootPartSo.SW && neighbours.TryGetValue(Direction.SouthWest, out var tileSW) &&
+                    !_tilesEvaluated.Contains(tileSW))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.SouthWest), tileSW,
                         freeJoinTiles);
-                if (rootPart.rootPartSo.W && neighbours.TryGetValue(Direction.West, out var tileW))
+                if (rootPart.rootPartSo.W && neighbours.TryGetValue(Direction.West, out var tileW) &&
+                    !_tilesEvaluated.Contains(tileW))
                     GetAllFreeJointTiles(DirectionValues.GetInverseDirection(Direction.West), tileW, freeJoinTiles);
             }
-
-            freeJoinTiles.Add(KeyValuePair.Create(inverseParentDirection, currentTile));
+            else
+            {
+                freeJoinTiles.Add(KeyValuePair.Create(inverseParentDirection, currentTile));
+            }
         }
 
-        public List<KeyValuePair<Direction, Tile>> GetPossibleTiles(RootPartSo rootPartSo)
+        public List<Tile> GetPossibleTiles(RootPartSo rootPartSo)
         {
-            var results = new List<KeyValuePair<Direction, Tile>>();
-            GetAllFreeJointTiles(Direction.NorthEast, startTile, results);
-            
-            
-            return results;
+            _tilesEvaluated.Clear();
+            var freeJointList = new List<KeyValuePair<Direction, Tile>>();
+            GetAllFreeJointTiles(Direction.NorthEast, startTile, freeJointList);
+            var result = new List<Tile>();
+
+            foreach (var freeJointKp in freeJointList)
+                if (
+                    (rootPartSo.NW && freeJointKp.Key == Direction.SouthEast)
+                    || (rootPartSo.NE && freeJointKp.Key == Direction.SouthWest)
+                    || (rootPartSo.E && freeJointKp.Key == Direction.West)
+                    || (rootPartSo.SE && freeJointKp.Key == Direction.NorthWest)
+                    || (rootPartSo.SW && freeJointKp.Key == Direction.NorthEast)
+                    || (rootPartSo.W && freeJointKp.Key == Direction.East)
+                )
+                    result.Add(freeJointKp.Value);
+
+
+            return result;
         }
     }
 }
